ff2dd47eccd53992c67e303420755d31027388af
Quoc113 <quoc113@gmail.com>
2026-03-19 15:32:37 +0700

    feat: add describe for data

--- code/Part1_Regression/utils.py
+++ code/Part1_Regression/utils.py
@@ -1,1 +1,11 @@
+import os   
 import pandas as pd
+
+def read_data(path, file_name):
+    file_path = os.path.join(path, file_name)
+    if os.path.exists(file_path):
+        print(f"Đang đọc dữ liệu từ {file_name}...")
+        return pd.read_csv(file_path)
+    else:
+        print(f"File {file_name} không tồn tại trong đường dẫn.")
+        return None
